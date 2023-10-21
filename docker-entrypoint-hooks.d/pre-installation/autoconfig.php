420dab11ec46d6ba9d3953c1df2b2ee0fc1c94d3
Jordan Williams <jordan@jwillikers.com>
2023-10-21 11:24:48 -0500

    Configure remaining options through the autoconfig.php file

---
+++ docker-entrypoint-hooks.d/pre-installation/autoconfig.php
@@ -0,0 +1,10 @@
+$AUTOCONFIG = array(
+  'filelocking.enabled' => true,
+  'default_phone_region' => 'US',
+  'enabledPreviewProviders' => 
+  array (
+    0 => 'OC\\Preview\\Imaginary',
+  ),
+  'preview_imaginary_url' => 'http://localhost:8088',
+  'trashbin_retention_obligation' => '7, 30',
+);
---
+++ docker-entrypoint-hooks.d/pre-installation/autoconfig.sh
@@ -0,0 +1,3 @@
+#/usr/bin/env sh
+
+cat /docker-entrypoint-hooks.d/pre-installation/autoconfig.php >> /var/www/html/autoconfig.php
